03c0f99087a69067dc5896f4199c9667eba06548
mblarson <mathewslarson@hotmail.com>
2026-01-17 10:34:49 -0400

    feat: Add sector J to SETORES_CAPITAL
    Includes the new sector 'J' in the list of capital sectors for order management.

--- constants.ts
+++ constants.ts
@@ -2,5 +2,5 @@
 export const INFANTIL_SIZES = ['1', '2', '4', '6', '8', '10', '12', '14'];
 export const BABYLOOK_SIZES = ['PP', 'P', 'M', 'G', 'GG', 'XGG'];
 export const UNISSEX_SIZES = ['PP', 'P', 'M', 'G', 'GG', 'XGG', 'XXGG (MAESTRO)'];
-export const SETORES_CAPITAL = ['A', 'B', 'C1', 'C2', 'D', 'E', 'F', 'G', 'H', 'I', 'M', 'N', 'UMADEMATS'];
+export const SETORES_CAPITAL = ['A', 'B', 'C1', 'C2', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'M', 'N', 'UMADEMATS'];
 export const DEFAULT_PRICE = 30.00;
